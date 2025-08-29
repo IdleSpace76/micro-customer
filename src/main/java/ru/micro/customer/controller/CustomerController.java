d347f001473573598a3c7c4350e3b6e03ba9ec68
Жаров Андрей <rougik3000@gmail.com>
2025-08-29 12:11:20 +0300

    В контроллер CustomerController добавлен метод на вывод всего списка Customer

--- src/main/java/ru/micro/customer/controller/CustomerController.java
+++ src/main/java/ru/micro/customer/controller/CustomerController.java
@@ -6,16 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.micro.customer.domain.Customer;
 import ru.micro.customer.repository.CustomerRepository;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 /**
  * @author a.zharov
@@ -49,5 +47,11 @@ public class CustomerController {
         headers.add("X-" + applicationName + "-alert", message);
         headers.add("X-" + applicationName + "-params", customer.getId());
         return ResponseEntity.created(new URI("/api/customers/" + result.getId())).headers(headers).body(result);
+    }
+
+    @GetMapping("/customers")
+    public List<Customer> getAllCustomers() {
+        log.debug("REST request to get all Customers");
+        return customerRepository.findAll();
     }
 }
